b801b94f0d239f0e4002c703f4983c5b83d7a7fa
Anthony C <broac@live.ca>
2015-12-27 23:41:14 -0800

    Changed AudioPage so that comedian name is formatted on separate lines via converter

---
+++ Comedian Soundboard.Shared/Converters/NameSplit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Comedian_Soundboard.Converters
+{
+    public class NameSplit : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var names = (value as string).Split(' ');
+            StringBuilder formattedName = new StringBuilder();
+            int count = 0;
+            while (count < names.Length) {
+                formattedName.Append(names[count++]);
+                if (count != names.Length)
+                    formattedName.Append("\n");
+            }
+            return formattedName.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
